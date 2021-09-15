4440f4643382f4c908045d4a3281f1ac2a3a8b95
hayui13072000 <hayui13072000@gmail.com>
2021-09-15 10:01:12 +0700

    native branch

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,11 +1,20 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+	@Query(value = "SELECT * FROM Student WHERE name = 'ha'", nativeQuery = true)
+	List<Student> findBooksWithLanguage();
+
+	@Query(value = "SELECT * FROM Book WHERE age > 5", nativeQuery = true)
+	List<Student> findBooksWithPaperback();
+
 
 }
