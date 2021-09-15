afb1faaadfb6d445e13b36454c2985df0cd3e242
hayui13072000 <hayui13072000@gmail.com>
2021-09-15 10:04:36 +0700

    jpql branch

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,11 +1,18 @@
 package com.example.demo.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-
+	@Transactional
+    @Modifying
+	@Query("delete from Student s where s.id = ?1")
+    void deleteStudent(int id);
 }
